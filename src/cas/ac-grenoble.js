dfc6076bd7e6a71b8c9dd8c4eb90499c66da70be
RedBoxing <thomas200574@gmail.com>
2021-12-08 13:14:29 +0100

    Use educonnect in ac-grenoble

--- src/cas/ac-grenoble.js
+++ src/cas/ac-grenoble.js
@@ -1,11 +1,11 @@
-const wayf = require('./generics/kdecole-wayf');
+const educonnect = require('./generics/kdecole-educonnect');
 
-module.exports = (url, account, username, password) => wayf({
+module.exports = (url, account, username, password) => educonnect({
     url,
     account,
     username,
     password,
 
     casUrl: 'cas.ent.auvergnerhonealpes.fr',
-    idp: 'GRE-ATS'
+    idp: 'EDU'
 });
